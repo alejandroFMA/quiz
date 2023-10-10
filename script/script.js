7db8a094465a34b447e07e71bf380639d36a43e4
alejandroFMA <alexfmarquez@gmail.com>
2023-10-10 22:21:14 +0200

    update/back button, fix validation

--- script/script.js
+++ script/script.js
@@ -2,71 +2,61 @@ console.log("Hello World");
 
 const respuestasCorrectas = {
     pregunta1: "frodo",
-    pregunta2:"elgris",
-    pregunta3:"amigo",
-    pregunta4:"smeagol",
-    pregunta5:"trancos",
-    pregunta6:"cabellos",
-    pregunta7:"pie",
-    pregunta8:"lee",
-    pregunta9:"tyler",
-    pregunta10:"mili"
-
+    pregunta2: "elgris",
+    pregunta3: "amigo",
+    pregunta4: "smeagol",
+    pregunta5: "trancos",
+    pregunta6: "cabellos",
+    pregunta7: "pie",
+    pregunta8: "lee",
+    pregunta9: "tyler",
+    pregunta10: "mili"
 }
 
-
-/*let imagenInput = document.getElementsByClassName("merry");
-
-imagenInput.addEventListener("click", (event)=> {
-  
-    let imagenes = document.querySelectorAll('img');
-    imagenes.forEach((imagen) => {imagen.classList.remove('oculto'); })
-       
-})*/
-
-
-let validarRespuestas= document.querySelector("button");
-let totalAciertos= 0
-validarRespuestas.style.backgroundColor="white";
-
+let validarRespuestas = document.querySelector("button");
+let totalAciertos = 0;
+validarRespuestas.style.backgroundColor = "white";
 
 validarRespuestas.addEventListener("click", (event) => {
-    
     event.preventDefault();
 
-    let respuestasElement = document.querySelectorAll('input:checked'); //eleccion usuario
-    for (let i = 0; i < respuestasElement.length; i++) {  //iteracion por todos los inputs
-        let respuesta = respuestasElement[i];
-        let labelElement = document.querySelector(`label[for=${respuesta.id}]`);
-        /*if (respuestaElement.length === 0) {
-            alert("¡Debes responder a todas las preguntas!");
-        }*/
+    let respuestasUsuario = document.querySelectorAll('input:checked');
 
-        if (respuesta.value == respuestasCorrectas[respuesta.name]) { //condicional. respuestas[name] == pregunta1,2,3...pero más general
-            labelElement.style.backgroundColor = "green";
-            labelElement.style.color = "black"
+    if (respuestasUsuario.length !== Object.keys(respuestasCorrectas).length) {
+        alert("¡Debes responder a todas las preguntas!");
+        return; //alerta si numero de respuesas no es igual que numero repsuestas objeto
+    }
+
+    for (let i = 0; i < respuestasUsuario.length; i++) {
+        let respuesta = respuestasUsuario[i];
+        let etiqueta = document.querySelector(`label[for=${respuesta.id}]`);
+
+        if (respuesta.value === respuestasCorrectas[respuesta.name]) {
+            etiqueta.style.backgroundColor = "green";
+            etiqueta.style.color = "black";
             console.log("¡Respuesta correcta!");
-            ++totalAciertos;
+            totalAciertos++;
         } else {
-            labelElement.style.backgroundColor = "red";
-            labelElement.style.color = "black";
+            etiqueta.style.backgroundColor = "red";
+            etiqueta.style.color = "black";
             console.log("¡Respuesta incorrecta!");
         }
+    }
 
-    } 
-
-    let h4 = document.createElement("h4")
-    let result = document.createTextNode(`Has tenido un total de ${totalAciertos} acierto/s`);
+    let h4 = document.createElement("h4");
+    let result = document.createTextNode(`Has acertado un total de: ${totalAciertos}`);
     h4.style.color = "white";
     h4.style.fontSize = "28px";
-    h4.style.backgroundColor="black";
 
     h4.appendChild(result);
-    document.getElementById("resultados").appendChild(h4)
-    let imagenes = document.querySelectorAll('img');
-    imagenes.forEach((imagen) => {imagen.classList.remove('oculto'); })
+    document.getElementById("resultados").appendChild(h4);
 
-})  
+    let imagenes = document.querySelectorAll('img');
+    imagenes.forEach((imagen) => {
+        imagen.classList.remove('oculto');
+    });
+});
+
 
 
 
